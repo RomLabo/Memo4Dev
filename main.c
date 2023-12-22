7f6ebe820a7d84e70830ef7ba46f24ecb2b00a2e
RomLabo <82595594+RomLabo@users.noreply.github.com>
2023-12-22 22:54:55 +0100

    ajout prise en compte des options en ligne de commande

--- main.c
+++ main.c
@@ -1,37 +1,73 @@
 /*
-0000000001 Author RomLabo
-1000111000 ReRad
-1000000001 Created on 24/11/2023.
-1000100011111000000001100001110000
-1000110001111000110001100010101000
-0000011000011000000001100011011000
+0000000001 Author RomLabo 111111111
+1000111000 Memo4Dev 111111111111111
+1000000001 Created on 22/12/2023 11
+10001000111110000000011000011100001
+10001100011110001100011000101010001
+00000110000110000000011000110110001
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* Globals constants */
+#define BADOPT -1
+
+typedef struct {
+    char *value;
+    int id;
+} m_opt;
+
+static m_opt all_opt[] = {
+    { "git", 0 }, { "js", 1 }, { "php", 2 }, { "css", 3 }
+};
+
+#define NBOPT (sizeof(all_opt)/sizeof(m_opt))
+
+/* 
+    Globals constants
+*/
 const unsigned short int USR_IN_SIZE = 2;
 const unsigned short int RES_YES = 0;
 const unsigned short int RES_NO = 1;
 const unsigned short int RES_ERR = 2;
 
-/* Globals variables */
+/*
+    Globals variables
+*/
 char usr_in_val[USR_IN_SIZE];
 int usr_in_bin = 0;
 int is_quit = RES_ERR;
 
-/* Declaration of functions */
+/* 
+    Declaration of functions
+*/
+int id_from_opt(const char *option);
 int do_quit(void);
 int empty_buffer(void);
 
-/* Main */
+/*
+    Main
+*/
 int main(int argc, const char * argv[]) {
-    if (argc > 0 && argc < 3) {
-        printf("%c\n", *(argv[1]));
+    printf("::: Memo4Dev :::\n\n");
+    int i;
+    
+    if (argc == 2) {
+        switch (id_from_opt(argv[1])) {
+            case 0: printf("call git\n"); break;
+            case 1: printf("call js\n"); break;
+            case 2: printf("call php\n"); break;
+            case 3: printf("call css\n"); break;
+            case -1: printf("Error \n"); break;
+        }
+    } else {
+        printf("all options :\n");
+        for (i = 0; i < NBOPT; i+= 2) {
+            printf("%s  %s\n", all_opt[i].value, all_opt[i+1].value);
+        }
     }
     
-    printf("Welcome on MemRo !!\n");
     int do_quit_val = 0;
     do {
         do_quit_val = do_quit();
@@ -48,11 +84,22 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+/*
+    Definition of functions
+*/
+int id_from_opt(const char *option) {
+    int i;
+    for (i=0; i < NBOPT; i++) {
+        m_opt *opt = &all_opt[i];
+        if (strcmp(opt->value, option) == 0)
+            return opt->id;
+    }
+    return BADOPT;
+}
 
-/* Definition of functions */
 int do_quit(void) {
     is_quit = RES_ERR;
-    printf("Quit MemRo (y/n) ? ");
+    printf("Quit (y/n) ? ");
     if (fgets(usr_in_val, USR_IN_SIZE, stdin) != NULL) {
         switch (usr_in_val[0]) {
             case 'y': is_quit = RES_YES; break;
